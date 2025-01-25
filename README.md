25192dda1edc5904ebe7aadfee19766413787426
Mkhitaryan Tatev <tatevmtg>
2025-01-25 14:06:06 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# cypressBDG"# DEMOQA" 
---
+++ cypress.config.js
@@ -0,0 +1,13 @@
+const { defineConfig } = require("cypress");
+const { downloadFile } = require("cypress-downloadfile/lib/addPlugin");
+require("dotenv").config();
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl: process.env.globalUrl,
+    setupNodeEvents(on, config) {
+      // Register the downloadFile task
+      on("task", { downloadFile });
+    },
+  },
+});
