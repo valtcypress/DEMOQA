d32a07813eadc72cb047e4cad0a7697de9705d99
#Grobo813663 <laurabolyan1192@gmail.com>
2025-01-27 22:18:07 -0800

    Web-Tables 1

---
+++ cypress/utils/data.js
@@ -0,0 +1,13 @@
+// export const LOCATORS = {
+
+// randomName: "Poghos",
+// randomLastName: "Poghosyan",
+// randomEmail: "poghos@example.com",
+// randomAddress: "Komitas street",
+// randomPermAddress: "Avan Arinj",
+// nameOutput: "Name:Poghos Poghosyan",
+// emailOutput: "Email:poghos@example.com",
+// addressOutput: "Current Address :Komitas street",
+// permAddressOutput: "Permananet Address :Avan Arinj",
+// invalidEmail: "poghosexample.com",
+// };
