fc63f9885d4231786cef820235bacbcef142975d
#Grobo813663 <laurabolyan1192@gmail.com>
2025-01-27 18:20:23 -0800

    TextBox firs commit

---
+++ cypress/Pages/TextBoxOfElements.js
@@ -0,0 +1,22 @@
+class TextBox {
+  NAMES = {
+    textBoxElements: "Elements",
+    textBoxLink: "Text Box",
+    textBoxTitle: "Text Box",
+    elementsLinkName: "/elements",
+    textBoxLinkName: "/text-box",
+  };
+
+  LOCATORS = {
+    heading: ".text-center",
+    mainTable: ".col-md-6",
+    table: ".col-md-9",
+    userName: "#userName",
+    userEmail: "#userEmail",
+    currentAddress: "#currentAddress",
+    permanentAddress: "#permanentAddress",
+    submit: "#submit",
+    output: "#output",
+  };
+}
+export const textBox = new TextBox();
