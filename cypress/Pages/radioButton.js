c2ec0df5126b8c63ce1778ede411301adce9c7b6
tatevmtg <tatevmtg>
2025-02-07 00:06:56 +0400

    radio button final changes

--- cypress/Pages/radioButton.js
+++ cypress/Pages/radioButton.js
@@ -7,15 +7,25 @@ class RadioButton {
     radioButtonNo: "No",
   };
 
-  LOCATORS = {
-    radioButtonOnPanel: "Radio Button",
-    radioButtonPageTitle: "Radio Button",
-    h1TitleRadioButton: ".text-center",
-    doYouLikeTheSite: ".mb-3",
-    radioButtonType: 'input[type="radio"]',
-    testAfterButtonCheck: ".text-success",
-    yourSelectedOption: ".mt-3",
-  };
+  h1TitleRadioButton() {
+    return cy.get(".text-center");
+  }
+
+  doYouLikeTheSite() {
+    return cy.get(".mb-3");
+  }
+
+  radioButtonType() {
+    return cy.get('input[type="radio"]');
+  }
+
+  testAfterButtonCheck() {
+    return cy.get(".text-success");
+  }
+
+  yourSelectedOption() {
+    return cy.get(".mt-3");
+  }
 }
 
 export const radioButton = new RadioButton();
